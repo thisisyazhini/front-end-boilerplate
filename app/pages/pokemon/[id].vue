bcb5a98e72272bdc85afc9b5908ffdabbdbe31dd
thisisyazhini <yazhihari3@gmail.com>
2025-02-09 12:44:51 +0100

    Type the Pokemon and RickAndMorty detail page

--- app/pages/pokemon/[id].vue
+++ app/pages/pokemon/[id].vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-import { pokemonImageBaseUrl } from '../../constants/index'
+import type { PokemonDetail } from '~/types/pokemon'
 
 const route = useRoute()
-const { data } = await usePokemonData(`pokemon/${route.params.id}`)
-const pokemonData = data.value as any
-const imageUrl = `${pokemonImageBaseUrl}/${pokemonData.id}.png`
+const { data } = await usePokemonData<PokemonDetail>(`pokemon/${route.params.id}`)
+const pokemonData = data.value
+const imageUrl = pokemonData?.sprites.other['official-artwork']?.front_default
 
-const items = computed(() => [{ label: 'Abilities', content: pokemonData.abilities.map(val => val.ability.name) }, { label: 'Forms', content: pokemonData.forms.map(val => val.name) }, { label: 'Moves', content: pokemonData.moves.map(val => val.move.name) }, { label: 'Type', content: pokemonData.types.map(val => val.type.name) }, { label: 'Stats', content: pokemonData.stats.map(val => val.stat.name) }])
-const gameIndicesList = computed(() => pokemonData.game_indices.map(val => ({ gameIndex: val.game_index, versionName: val.version.name, versionUrl: val.version.url })))
+const items = computed(() => [
+  { label: 'Abilities', content: pokemonData?.abilities.map(val => val.ability.name) },
+  { label: 'Forms', content: pokemonData?.forms.map(val => val.name) },
+  { label: 'Moves', content: pokemonData?.moves.map(val => val.move.name) },
+  { label: 'Type', content: pokemonData?.types.map(val => val.type.name) },
+  { label: 'Stats', content: pokemonData?.stats.map(val => val.stat.name) },
+])
+const gameIndicesList = computed(() =>
+  pokemonData?.game_indices.map(val =>
+    ({ gameIndex: val.game_index, versionName: val.version.name, versionUrl: val.version.url }),
+  ),
+)
 const page = ref(1)
 const pageCount = 5
 
 const gameIndicesListRows = computed(() => {
-  return gameIndicesList.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+  return gameIndicesList.value?.slice((page.value - 1) * pageCount, (page.value) * pageCount)
 })
 </script>
 
@@ -21,36 +31,32 @@ const gameIndicesListRows = computed(() => {
     <NuxtImg :src="imageUrl" sizes="50vw" class="h-full object-contain" />
     <div class="overflow-y-auto p-8">
       <h1 class="text-3xl font-bold capitalize text-center">
-        {{ pokemonData.name }}
+        {{ pokemonData?.name }}
       </h1>
       <div>
         <div class="flex align-middle justify-between text-md gap-4 mt-4">
           <div class="space-y-2">
             <label class="font-bold">Weight</label>
-            <div>{{ pokemonData.weight }}</div>
+            <div>{{ pokemonData?.weight }}</div>
           </div>
 
           <div class="space-y-2">
             <label class="font-bold">Height</label>
-            <div>{{ pokemonData.height }}</div>
+            <div>{{ pokemonData?.height }}</div>
           </div>
 
           <div class="space-y-2">
             <label class="font-bold">Order</label>
-            <div>{{ pokemonData.order }}</div>
+            <div>{{ pokemonData?.order }}</div>
           </div>
 
           <div class="space-y-2">
             <label class="font-bold">Base experience</label>
-            <div>{{ pokemonData.base_experience }}</div>
+            <div>{{ pokemonData?.base_experience }}</div>
           </div>
         </div>
         <UAccordion
-          class="py-6"
-          color="white"
-          :ui="{ item: { color: 'white' } }"
-          variant="ghost"
-          size="md"
+          class="py-6" color="white" :ui="{ item: { color: 'white' } }" variant="ghost" size="md"
           :items="items"
         >
           <template #item="{ item }">
@@ -60,14 +66,15 @@ const gameIndicesListRows = computed(() => {
           </template>
         </UAccordion>
         <UTable
-          :ui=" { base: 'bg-neutral-900',
-                  td: {
-                    color: 'text-white',
-                  } }"
-          :rows="gameIndicesListRows"
+          :ui="{
+            base: 'bg-black border border-primary',
+            td: {
+              color: 'text-white',
+            },
+          }" :rows="gameIndicesListRows"
         />
-        <div class="flex justify-center px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-          <UPagination v-model="page" :page-count="pageCount" :total="gameIndicesList.length" />
+        <div class="flex justify-center px-3 py-3.5">
+          <UPagination v-model="page" :page-count="pageCount" :total="gameIndicesList?.length || 0" />
         </div>
       </div>
     </div>
--- app/pages/rick-and-morty/[id].vue
+++ app/pages/rick-and-morty/[id].vue
@@ -1,26 +1,33 @@
 <script setup lang="ts">
+import type { RickAndMorty } from '~/types/rick-and-morty'
+
 const route = useRoute()
 
-const { data } = await useRickAndMortyData(`character/${route.params.id}`)
-const rickAndMortyData = data?.value as any
+const { data } = await useRickAndMortyData<RickAndMorty>(`character/${route.params.id}`)
+const rickAndMortyData = data?.value
 
-const items = computed(() => [{ label: 'Status', content: rickAndMortyData.status }, { label: 'Species', content: rickAndMortyData.species }, { label: 'Gender', content: rickAndMortyData.gender }, { label: 'Type', content: rickAndMortyData.type }])
+const items = computed(() => [
+  { label: 'Status', content: rickAndMortyData?.status },
+  { label: 'Species', content: rickAndMortyData?.species },
+  { label: 'Gender', content: rickAndMortyData?.gender },
+  { label: 'Type', content: rickAndMortyData?.type },
+])
 
-const episodeList = computed(() => rickAndMortyData.episode.map((val: string) => ({ episodeLink: val })))
+const episodeList = computed(() => rickAndMortyData?.episode.map((val: string) => ({ episodeLink: val })))
 const page = ref(1)
 const pageCount = 5
 
 const episodeListRows = computed(() => {
-  return episodeList.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+  return episodeList.value?.slice((page.value - 1) * pageCount, (page.value) * pageCount)
 })
 </script>
 
 <template>
   <div class="grid grid-cols-2 w-full bg-black text-white">
-    <NuxtImg :src="rickAndMortyData.image" sizes="50vw" class="h-full object-contain" />
+    <NuxtImg :src="rickAndMortyData?.image" sizes="50vw" class="h-full object-contain" />
     <div class="overflow-y-auto p-8">
       <h1 class="text-3xl font-bold text-center">
-        {{ rickAndMortyData.name }}
+        {{ rickAndMortyData?.name }}
       </h1>
       <div class="flex align-middle justify-between text-md gap-4 mt-4">
         <div class="space-y-2">
@@ -28,13 +35,13 @@ const episodeListRows = computed(() => {
             Origin
           </label>
           <div>
-            {{ rickAndMortyData.origin.name }}
+            {{ rickAndMortyData?.origin.name }}
           </div>
         </div>
 
         <div class="space-y-2">
           <label class="font-bold">Location</label>
-          <div>{{ rickAndMortyData.location.name }}</div>
+          <div>{{ rickAndMortyData?.location.name }}</div>
         </div>
       </div>
       <UAccordion
@@ -52,14 +59,16 @@ const episodeListRows = computed(() => {
         </template>
       </UAccordion>
       <UTable
-        :ui=" { base: 'bg-neutral-900',
-                td: {
-                  color: 'text-white',
-                } }"
+        :ui="{
+          base: 'bg-black border border-primary',
+          td: {
+            color: 'text-white',
+          },
+        }"
         :rows="episodeListRows"
       />
-      <div class="flex justify-center px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-        <UPagination v-model="page" :page-count="pageCount" :total="episodeList.length" />
+      <div class="flex justify-center px-3 py-3.5 ">
+        <UPagination v-model="page" :page-count="pageCount" :total="episodeList?.length || 0" />
       </div>
     </div>
   </div>
