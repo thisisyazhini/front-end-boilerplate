a5a207c5550153739c814d77a5866eae5a810d7b
thisisyazhini <yazhihari3@gmail.com>
2025-02-10 09:03:53 +0100

    Fix NuxtImg on Netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,10 @@ export default defineNuxtConfig({
       routes: ['/', '/assignment'],
     },
   },
-
+  image: {
+    provider: 'netlify',
+    domains: ['raw.githubusercontent.com', 'rickandmortyapi.com'],
+  },
   app: {
     head: {
       viewport: 'width=device-width,initial-scale=1',
